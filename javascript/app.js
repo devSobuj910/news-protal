dc9d671510634ebfa2307cd9a77ae4bbe63eb2f6
devSobuj910 <ftsobuj222@gmail.com>
2022-09-03 03:05:36 +0600

    load data working

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,12 @@
-console.log("connect");
+const loadData = async () => {
+  try {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+loadData();
