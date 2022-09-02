4917deaf833961aa8e6e975cd059f92a3452cd19
devSobuj910 <ftsobuj222@gmail.com>
2022-09-03 05:14:36 +0600

    car show succecfully

--- javascript/app.js
+++ javascript/app.js
@@ -11,7 +11,6 @@ const loadCatagory = async () => {
 
 const catagoty = (data) => {
   const navUl = document.getElementById("navbar-menu");
-  console.log(data);
 
   data.forEach((sData) => {
     const creatElemet = document.createElement("li");
@@ -34,9 +33,67 @@ const loadNews = (category_id) => {
   console.log(url);
   fetch(url)
     .then((res) => res.json())
-    .then((data) => showNews(data));
+    .then((data) => showNews(data.data));
 };
 
 const showNews = (data) => {
   console.log(data);
+  const cardParent = document.getElementById("card-parent");
+
+  data.forEach((news) => {
+    const cretcardDiv = document.createElement("div");
+    cretcardDiv.classList.add("card");
+    cretcardDiv.innerHTML = `
+    
+      <div class="card mb-3">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="..." class="img-fluid rounded-start" alt="..." />
+                </div>
+                <div class="col-md-8">
+
+
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                  </div>
+                  <div class="card-footer d-flex">
+                    <div  class="author d-flex">
+                      <img id="author" src="/images/user.png" alt="" />
+                      <h4>name: sakila</h4>
+                      <p>date:1.2.2.3</p>
+                    </div>
+                    <div class="viw d-flex"><i class="fa-solid fa-eye"></i></span>1.6M</div>
+                    <div class="ratting d-flex">
+                      <li><i class="fa-solid fa-star"></i></li>
+                      <li><i class="fa-solid fa-star"></i></li>
+                      <li><i class="fa-solid fa-star"></i></li>
+                      <li><i class="fa-solid fa-star"></i></li>
+                      <li><i class="fa-solid fa-star-half-stroke"></i></li>
+                      <div class="arow">
+                        <li><i class="fa-solid fa-arrow-right"></i></li>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+
+
+
+                
+              </div>
+            </div>
+    
+    
+    
+    
+    `;
+
+    cardParent.appendChild(cretcardDiv);
+  });
 };
