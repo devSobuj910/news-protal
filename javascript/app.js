b7cd98c2a10e29b0165cbb5772a9141e5309724a
devSobuj910 <ftsobuj222@gmail.com>
2022-09-03 03:33:49 +0600

    catagory set

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,26 @@
-const loadData = async () => {
+const loadCatagory = async () => {
   try {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
+    const datas = await res.json();
+    console.log(datas);
+    catagoty(datas.data.news_category);
   } catch (err) {
     console.log(err);
   }
 };
 
-loadData();
+const catagoty = (data) => {
+  console.log(data);
+
+  const navUl = document.getElementById("navbar-menu");
+
+  data.forEach((sData) => {
+    const creatElemet = document.createElement("li");
+    creatElemet.classList.add("nav-item");
+    creatElemet.innerHTML = sData.category_name;
+    navUl.appendChild(creatElemet);
+  });
+};
+
+loadCatagory();
